d17e7fcab936474c861294a9c734249af2594004
Ayesha Khan <ayesha_2101ph12@iitp.ac.in>
2024-11-06 21:10:21 +0530

    "configuring PORT"

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 from your_module import get_fashion_recommendations, filter_csv_and_display_images
-
+import os
 
 app = Flask(__name__)
 
@@ -32,4 +32,5 @@ def upload_file():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
